120707d1ef48a281a535cd21d87b611557e0d2b8
NeiraJ <johnny.neira@gmail.com>
2024-08-29 18:13:26 +0200

    Ajout de la documentation pour la classe Ingredient
    - Implémentation de la méthode __init__()
    - Ajout de la description de la méthode __init__() dans le docstring

--- src/ingredients/ingredient.py
+++ src/ingredients/ingredient.py
@@ -4,6 +4,12 @@ from abc import ABC
 class Ingredient(ABC):
 
     def __init__(self, nom: str, quantite: float, unite: str):
+        """
+
+        :param nom:
+        :param quantite:
+        :param unite:
+        """
         self.nom = nom
         self._quantite = quantite
         self.unite = unite
