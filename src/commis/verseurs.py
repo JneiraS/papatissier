737d0fa65ff1f6adaaeb2f40993248d10a79b365
NeiraJ <johnny.neira@gmail.com>
2024-08-29 12:49:03 +0200

    Ajout de la classe Verseur pour la préparation des ingrédients
    - Création de la classe Verseur héritant de Commis et threading.Thread
    - Implémentation de la logique de préparation des ingrédients

---
+++ src/commis/verseurs.py
@@ -0,0 +1,24 @@
+import math
+import threading
+import time
+
+from src.commis.commis import Commis
+from src.recipients.recipient import Recipient
+
+
+class Verseurs(Commis, threading.Thread):
+    def __init__(self, recipient: Recipient, quantite=None):
+        threading.Thread.__init__(self)
+        self.recipient = recipient
+
+    def run(self):
+        print("Je mets de l'eau à chauffer dans une bouilloire")
+        time.sleep(8)
+        print("Je verse l'eau dans une casserole")
+        time.sleep(2)
+        print("J'y pose le bol rempli de chocolat")
+        time.sleep(1)
+        nb_tours = math.ceil(self.quantite / 10)
+        for no_tour in range(1, nb_tours + 1):
+            print(f"Je mélange {self.quantite} de chocolat à fondre, tour n°{no_tour}")
+            time.sleep(.1)  # temps supposé d'un tour de spatule
