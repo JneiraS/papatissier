d6577d1074311232c60c4816faefbd12def67353
NeiraJ <johnny.neira@gmail.com>
2024-08-29 18:12:45 +0200

    Amélioration de la structure et de la documentation du script principal
    - Ajout de commentaires explicatifs pour la fonction main()
    - Organisation des différentes parties du script (recipients, ingrédients, commis, processus)
    - Correction de la syntaxe et amélioration de la lisibilité du code

--- main.py
+++ main.py
@@ -7,42 +7,45 @@ from src.ingredients.chocolat import Chocolat
 from src.ingredients.oeuf import Oeuf
 from src.recipients.concret_recipients import ConcretRecipient
 
+
 def main():
-    # Recipients
+    """
+    Point d'entrée principal du programme.
+    Cette fonction initialise les participants, les ingrédients et les commis.
+    Enfin, elle imprime le résultat de la « préparation ».
+    """
+    # Recipients ----------------
     cul_de_poule_pour_les_oeufs = ConcretRecipient("Cul de poule")
     bol_pour_le_chocolat_1 = ConcretRecipient("Bol")
     bol_pour_le_chocolat_2 = ConcretRecipient("Bol")
 
-    # Ingredients
+    # Ingredients ----------------
     oeufs = Oeuf(6, "unite")
     chocolat_1 = Chocolat(200, "g")
     chocolat_2 = Chocolat(200, "g")
 
-    # Commis
+    # Commis ----------------
     batteur = BatteurOeufs(oeufs, cul_de_poule_pour_les_oeufs)
     fondeur_chocolat_1 = FondeurChocolat(chocolat_1, bol_pour_le_chocolat_1)
     fondeur_chocolat_2 = FondeurChocolat(chocolat_2, bol_pour_le_chocolat_2)
     verseur_1 = Verseurs(bol_pour_le_chocolat_1, cul_de_poule_pour_les_oeufs)
     verseur_2 = Verseurs(bol_pour_le_chocolat_2, cul_de_poule_pour_les_oeufs)
 
-    # Process
+    # Process ----------------
     batteur.start()
     fondeur_chocolat_1.start()
     fondeur_chocolat_2.start()
-
     batteur.join()
     fondeur_chocolat_1.join()
     fondeur_chocolat_2.join()
-
     print("\nJe peux à présent incorporer le chocolat aux oeufs\n")
-
     verseur_1.start()
     verseur_2.start()
-
     verseur_1.join()
     verseur_2.join()
 
-    print("\n\nLe Cul de poule contient un melange de: \n")
+    # Affichage du resultat ----------------
+    print(f"\n\nLe {cul_de_poule_pour_les_oeufs.name} un melange de: \n")
     for element in cul_de_poule_pour_les_oeufs.sum_of_quantite():
         print(element)
 
