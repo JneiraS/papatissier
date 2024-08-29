eb41cb2ee231a4cbd29a78b548195a678a97bf6d
NeiraJ <johnny.neira@gmail.com>
2024-08-29 17:26:55 +0200

    Ajout de méthodes supplémentaires à Recipient et conversion d'Ingredient à Appareil
    - Implémentation de la méthode convert_ingredient_to_appareil
    - Ajout de la méthode sum_of_quantite pour calculer la somme des quantités
    - Amélioration de la gestion de l'affichage des recipients

--- src/recipients/recipient.py
+++ src/recipients/recipient.py
@@ -1,12 +1,26 @@
 from abc import ABC
 
+from src.Appareil import Appareil
 from src.ingredients.ingredient import Ingredient
 
 
 class Recipient(ABC):
-    def __init__(self, name: str, contient: Ingredient):
+    def __init__(self, name: str, contient: Ingredient | Appareil):
         self.name = name
         self.contient = contient
 
-    def __str__(self):
-        return f"{self.name} {self.contient}"
+    def convert_ingredient_to_appareil(self):
+
+        if isinstance(self.contient, Ingredient):
+            ingredient = self.contient
+            self.contient = Appareil()
+            self.contient.add_ingredient(ingredient)
+
+    def sum_of_quantite(self):
+        total = 0
+        if isinstance(self.contient, Appareil):
+            for qty in self.contient.composition:
+                total += qty.quantite
+            return total
+        else:
+            return self.contient.quantite
