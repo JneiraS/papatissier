9abff525a789d5ce48c1e2a8f7c8aed2382acb17
NeiraJ <johnny.neira@gmail.com>
2024-08-29 12:45:28 +0200

    Préparation de la classe Commis pour l'héritage
    - Préparation de la classe Commis pour hériter de threading.Thread
    - Ajout de la méthode run() abstraite

--- src/commis/commis.py
+++ src/commis/commis.py
@@ -1,15 +1,8 @@
 import threading
 from abc import ABC, abstractmethod
 
-from src.recipients.recipient import Recipient
-
 
 class Commis(threading.Thread, ABC):
-
-    def __init__(self, quantite, recipient: Recipient):
-        self.quantite = quantite
-        self.recipient = recipient
-        threading.Thread.__init__(self)
 
     @abstractmethod
     def run(self):
