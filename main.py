494df3d71cfb81593b3bdc032a6e3ad749cdd2d9
NeiraJ <johnny.neira@gmail.com>
2024-08-29 12:44:31 +0200

    Ajout de la gestion des recipients concrets et simplification de la logique de fabrication
    - Remplacement de CulDePoule par ConcretRecipient pour les recipients
    - Ajout de Verseur pour la phase de préparation
    - Simplification de la création des commis en utilisant directement les ingredients
    - Modification de la logique de fabrication pour inclure les deux recipients de chocolat
    - Ajout de la vérification finale des recipients après la fabrication

--- main.py
+++ main.py
@@ -4,25 +4,45 @@ from src.commis.batteur import BatteurOeufs
 from src.commis.fondeur import FondeurChocolat
 from src.ingredients.chocolat import Chocolat
 from src.ingredients.oeuf import Oeuf
-from src.recipients.cul_de_poule import CulDePoule
+from src.recipients.cul_de_poule import ConcretRecipient
+
+
+class Verseur:
+    pass
 
 
 def main():
     # Recipients
-    cul_de_poule_pour_les_oeufs = CulDePoule("Cul de poule")
-    cul_de_poule_pour_le_chocolat = CulDePoule("Cul de poule")
+    cul_de_poule_pour_les_oeufs = ConcretRecipient("Cul de poule")
+    bol_pour_le_chocolat_1 = ConcretRecipient("Bol")
+    bol_pour_le_chocolat_2 = ConcretRecipient("Bol")
 
     # Ingredients
     oeufs = Oeuf(6, "unite")
     chocolat = Chocolat(200, "g")
 
-    batteur = BatteurOeufs(oeufs.quantite, cul_de_poule_pour_les_oeufs)
-    fondeur = FondeurChocolat(chocolat.quantite, cul_de_poule_pour_le_chocolat)
+    # Commis
+    batteur = BatteurOeufs(oeufs, cul_de_poule_pour_les_oeufs)
+    fondeur_chocolat_1 = FondeurChocolat(chocolat, bol_pour_le_chocolat_1)
+    fondeur_chocolat_2 = FondeurChocolat(chocolat, bol_pour_le_chocolat_2)
+    verseur_1 = Verseur()
+    verseur_1 = Verseur()
+
+    # Process
+
     batteur.start()
-    fondeur.start()
+    fondeur_chocolat_1.start()
+    fondeur_chocolat_2.start()
+
     batteur.join()
-    fondeur.join()
-    print("\nJe peux à présent incorporer le chocolat aux oeufs")
+    fondeur_chocolat_1.join()
+    fondeur_chocolat_2.join()
+
+    print("\nJe peux à présent incorporer le chocolat aux oeufs\n")
+
+    print(bol_pour_le_chocolat_1)
+    print(bol_pour_le_chocolat_2)
+    print(cul_de_poule_pour_les_oeufs)
 
 
 if __name__ == "__main__":
