128d10811f54beebf65b5e1057024859f6bf7d88
NeiraJ <johnny.neira@gmail.com>
2024-08-29 12:48:55 +0200

    Ajout de l'interface Ingredient et mise à jour de Recipient
    - Import de l'interface Ingredient
    - Modification de Recipient pour gérer un ingrédient
    - Implémentation de la méthode __str__ pour une meilleure représentation

--- src/recipients/recipient.py
+++ src/recipients/recipient.py
@@ -1,6 +1,12 @@
 from abc import ABC
 
+from src.ingredients.ingredient import Ingredient
+
 
 class Recipient(ABC):
-    def __init__(self, name: str):
+    def __init__(self, name: str, contient: Ingredient):
         self.name = name
+        self.contient = contient
+
+    def __str__(self):
+        return f"{self.name} {self.contient}"
