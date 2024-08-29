9f91858c33849dbc48770e95cee305be4877eaf7
NeiraJ <johnny.neira@gmail.com>
2024-08-29 18:13:35 +0200

    Amélioration de la documentation et de la gestion de la propriété quantite
    - Modification de la méthode __init__() pour inclure les paramètres de quantité et unité
    - Ajout de la description de la méthode __init__() dans le docstring
    - Conversion de la propriété quantite en méthode get

--- src/ingredients/oeuf.py
+++ src/ingredients/oeuf.py
@@ -4,8 +4,23 @@ from src.ingredients.ingredient import Ingredient
 class Oeuf(Ingredient):
 
     def __init__(self, quantite: float, unite: str):
+        """
+        Initialisation d'un oeuf
+
+        :param quantite: la quantit  de l'oeuf
+        :type quantite: float
+        :param unite: l'unit  de la quantit  de l'oeuf
+        :type unite: str
+        """
+
         super().__init__("oeuf", quantite, unite)
 
     @property
-    def quantite(self):
+    def quantite(self) -> float:
+        """
+        Get the quantity of eggs
+
+        :return: the quantity of eggs
+        :rtype: float
+        """
         return self._quantite
