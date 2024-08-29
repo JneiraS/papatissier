986ba216db244fd00c8db726d05f6a10eaf857e7
NeiraJ <johnny.neira@gmail.com>
2024-08-29 17:26:37 +0200

    Modification de ConcretRecipient pour gérer l'Appareil en plus des ingrédients
    - Ajout de l'Appareil comme paramètre optionnel dans le constructeur
    - Implémentation de la méthode sum_of_quantite

--- src/recipients/concret_recipients.py
+++ src/recipients/concret_recipients.py
@@ -1,12 +1,9 @@
+from src.Appareil import Appareil
 from src.ingredients.ingredient import Ingredient
 from src.recipients.recipient import Recipient
 
 
 class ConcretRecipient(Recipient):
 
-    def __init__(self, name: str, contient: Ingredient = None):
+    def __init__(self, name: str, contient: Ingredient | Appareil | None = None):
         super().__init__(name, contient)
-
-    def __str__(self):
-        return (f"{self.name} contenant: {self.contient.quantite} "
-                f"{self.contient.unite} de {self.contient.nom}")
