eead25aa1e86d45bfc558bb4a0d1b91b5155a6c4
NeiraJ <johnny.neira@gmail.com>
2024-08-29 17:25:36 +0200

    Modification de la logique de fabrication et ajout de Verseurs
    - Ajout de Verseurs pour la préparation des ingrédients
    - Utilisation de deux lots de chocolat plutôt qu'un seul
    - Modification de la logique de finition pour utiliser les Verseurs
    - Ajout de la vérification finale des quantités totales dans le recipient final

--- main.py
+++ main.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 from src.commis.batteur import BatteurOeufs
 from src.commis.fondeur import FondeurChocolat
+from src.commis.verseurs import Verseurs
 from src.ingredients.chocolat import Chocolat
 from src.ingredients.oeuf import Oeuf
 from src.recipients.concret_recipients import ConcretRecipient
 
-
-class Verseur:
-    pass
 
 
 def main():
@@ -19,17 +17,17 @@ def main():
 
     # Ingredients
     oeufs = Oeuf(6, "unite")
-    chocolat = Chocolat(200, "g")
+    chocolat_1 = Chocolat(200, "g")
+    chocolat_2 = Chocolat(200, "g")
 
     # Commis
     batteur = BatteurOeufs(oeufs, cul_de_poule_pour_les_oeufs)
-    fondeur_chocolat_1 = FondeurChocolat(chocolat, bol_pour_le_chocolat_1)
-    fondeur_chocolat_2 = FondeurChocolat(chocolat, bol_pour_le_chocolat_2)
-    verseur_1 = Verseur()
-    verseur_1 = Verseur()
+    fondeur_chocolat_1 = FondeurChocolat(chocolat_1, bol_pour_le_chocolat_1)
+    fondeur_chocolat_2 = FondeurChocolat(chocolat_2, bol_pour_le_chocolat_2)
+    verseur_1 = Verseurs(bol_pour_le_chocolat_1, cul_de_poule_pour_les_oeufs)
+    verseur_2 = Verseurs(bol_pour_le_chocolat_2, cul_de_poule_pour_les_oeufs)
 
     # Process
-
     batteur.start()
     fondeur_chocolat_1.start()
     fondeur_chocolat_2.start()
@@ -40,9 +38,13 @@ def main():
 
     print("\nJe peux à présent incorporer le chocolat aux oeufs\n")
 
-    print(bol_pour_le_chocolat_1)
-    print(bol_pour_le_chocolat_2)
-    print(cul_de_poule_pour_les_oeufs)
+    verseur_1.start()
+    verseur_2.start()
+
+    verseur_1.join()
+    verseur_2.join()
+
+    print(cul_de_poule_pour_les_oeufs.sum_of_quantite())
 
 
 if __name__ == "__main__":
