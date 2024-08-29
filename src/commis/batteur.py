eccfbd48f62c91dad8d3568c90bc867bf7631e22
NeiraJ <johnny.neira@gmail.com>
2024-08-29 12:44:42 +0200

    Mise à jour de la classe BatteurOeufs pour utiliser l'interface Ingredient
    - Modification de l'initialisation pour accepter un objet Ingredient au lieu d'une quantité
    - Ajout de la gestion de la quantité d'œufs dans le recipient final
    - Optimisation du nombre de tours de batteur

--- src/commis/batteur.py
+++ src/commis/batteur.py
@@ -2,17 +2,19 @@ import threading
 import time
 
 from src.commis.commis import Commis
+from src.ingredients.ingredient import Ingredient
 from src.recipients.recipient import Recipient
 
 
 class BatteurOeufs(Commis, threading.Thread):
-    def __init__(self, nb_oeufs: int, recipient: Recipient):
+    def __init__(self, ingredient: Ingredient, recipient: Recipient):
         threading.Thread.__init__(self)
-        self.nb_oeufs = nb_oeufs
+        self.nb_oeufs = ingredient
         self.recipient = recipient
 
     def run(self):
-        nb_tours = self.nb_oeufs * 8
+        nb_tours = self.nb_oeufs.quantite * 8
         for no_tour in range(1, nb_tours + 1):
-            print(f"\tJe bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
-            time.sleep(0.5)  # temps supposé d'un tour de batteur
+            print(f"\tJe bats les {self.nb_oeufs.quantite} oeufs, tour n°{no_tour}")
+            time.sleep(0.1)  # temps supposé d'un tour de batteur
+        self.recipient.contient = self.nb_oeufs
